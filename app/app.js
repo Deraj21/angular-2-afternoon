fce439f81b373c87b9d1222b538780a45558e583
deraj21 <deraj21@gmail.com>
2018-07-21 09:39:21 -0700

    done through step 4; all route scope tests pass

--- app/app.js
+++ app/app.js
@@ -3,8 +3,37 @@ angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider,
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
+    })
+    .state('packages', {
+      url: '/packages/:country',
+      templateUrl: 'app/packages/packagesTmpl.html',
+      controller: 'packagesCtrl'
+    })
+    .state('locations', {
+      url: '/locations',
+      templateUrl: 'app/locations/locationsTmpl.html',
+      controller: 'locationsCtrl'
+    })
+    .state('booked', {
+      url: '/booked/:id',
+      templateUrl: 'app/booked/bookedTmpl.html',
+      controller: 'bookedCtrl'
+    })
+    .state('adventures', {
+      url: '/adventures',
+      templateUrl: 'app/about/adventures/adventuresTmpl.html',
+      parent: 'home'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'app/contact/contactTmpl.html',
+      parent: 'home'
     });
 
   $urlRouterProvider
     .otherwise('/');
 });
+
+/**
+
+ */
