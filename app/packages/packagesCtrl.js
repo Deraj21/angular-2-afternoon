9f26673a7be0f54d29a9d56e9ed4ef85d2137e2e
deraj21 <deraj21@gmail.com>
2018-07-31 15:32:52 -0700

    all done?

--- app/packages/packagesCtrl.js
+++ app/packages/packagesCtrl.js
@@ -5,12 +5,3 @@ angular.module('devmtnTravel').controller('packagesCtrl', function($scope, $stat
       $scope.packages = $scope.allPackages.filter(item => item.country === $stateParams.country);
     })
 });
-
-/**
-city - String.
-country - String.
-desc - String.
-id - Number.
-image - String ( a filepath to a specific image in the img/ folder ).
-price - Number.
- */
